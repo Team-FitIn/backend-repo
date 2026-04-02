21383d8ed2383cc4384e808626280d6e3851206b
Chae, KyungWan <house05240@naver.com>
2026-04-02 11:13:41 +0900

    feat: 초기 프로젝트 구성 및 DB 엔티티 설계 완료

---
+++ src/main/groovy/team/fitin/FitInApplication.groovy
@@ -0,0 +1,13 @@
+package team.fitin
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class FitInApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(FitInApplication, args)
+    }
+
+}
