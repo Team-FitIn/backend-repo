4c366086c4c33f0026cb6016241e85c6e19adb8f
Chae, KyungWan <house05240@naver.com>
2026-04-02 15:35:53 +0900

    feat: GarmentRepository 구현 의류 조회

---
+++ src/main/groovy/team/fitin/repository/GarmentRepository.java
@@ -0,0 +1,16 @@
+package team.fitin.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import team.fitin.domain.Garment;
+import java.util.List;
+
+@Repository
+public interface GarmentRepository extends JpaRepository<Garment, Long> {
+
+    // 특정 카테고리(상의, 하의 등)의 옷들만 모아보기
+    List<Garment> findByCategoryMain(String categoryMain);
+
+    // 브랜드 이름으로 검색하기
+    List<Garment> findByBrand(String brand);
+}
