2fb1e461892188844de2e38138175100c8deb1c4
Chae, KyungWan <house05240@naver.com>
2026-04-08 17:10:45 +0900

    fix: import 부분 변경 필요없는 Garment 삭제

--- src/main/groovy/team/fitin/web/GarmentController.java
+++ src/main/groovy/team/fitin/web/GarmentController.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import team.fitin.domain.Garment;
 import team.fitin.dto.GarmentResponseDto;
 import team.fitin.service.GarmentService;
 
