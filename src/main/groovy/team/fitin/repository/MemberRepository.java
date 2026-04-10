3db7c4a93fd3b5e227f610e7b6095fa329b3ba96
Chae, KyungWan <house05240@naver.com>
2026-04-10 15:50:41 +0900

    feat: 이메일로 찾아야할 유저찾는 메소드 추가

--- src/main/groovy/team/fitin/repository/MemberRepository.java
+++ src/main/groovy/team/fitin/repository/MemberRepository.java
@@ -3,8 +3,9 @@ package team.fitin.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import team.fitin.domain.Member;
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    //Optional<Member> findByEmail(String email); 나중에 이메일로 찾아야할시 사용가능
+    Optional<Member> findByEmail(String email);
 }
