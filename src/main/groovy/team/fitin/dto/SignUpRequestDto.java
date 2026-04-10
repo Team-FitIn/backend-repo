5d2c134f5645c901104d20d303528d29f4a26208
Chae, KyungWan <house05240@naver.com>
2026-04-10 17:21:36 +0900

    feat: SignUpRequestDto 생성 빠른 회원가입을 위해 어깨너비 다리길이는 생략 후에 회원정보에서 기입, 키랑 몸무게도 없애는 좋을것 같긴 함(회의 후 정하는 걸로)

---
+++ src/main/groovy/team/fitin/dto/SignUpRequestDto.java
@@ -0,0 +1,13 @@
+package team.fitin.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class SignUpRequestDto {
+    private String email;
+    private String password;
+    private Float height;
+    private Float weight;
+}
