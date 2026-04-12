50ce16b9747e0741995c0faf39651f3934365c1b
Chae, KyungWan <house05240@naver.com>
2026-04-13 02:17:57 +0900

    feat: getMemberInfo 로직 추가
    이메일로 회원을 조회하여 DTO로 반환합니다

--- src/main/groovy/team/fitin/service/MemberService.java
+++ src/main/groovy/team/fitin/service/MemberService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.fitin.domain.Member;
 import team.fitin.dto.BodyInfoRequestDto;
+import team.fitin.dto.MemberResponseDto;
 import team.fitin.repository.MemberRepository;
 
 @Service
@@ -25,5 +26,14 @@ public class MemberService {
                 requestDto.getShoulderWidth(),
                 requestDto.getLegLength()
         );
+    }
+    /**
+     * 이메일로 회원을 조회하여 DTO로 반환합니다.
+     */
+    public MemberResponseDto getMemberInfo(String email) {
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("해당 회원을 찾을 수 없습니다."));
+
+        return MemberResponseDto.from(member);
     }
 }
