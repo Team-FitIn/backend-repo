0176f11fea14dfbe0b32ff0306c1b0df11aa4008
Chae, KyungWan <house05240@naver.com>
2026-04-13 01:51:08 +0900

    feat: BodyInfoRequestDto 구현
    수정할 신체정보만 담는 DTO 구현

---
+++ src/main/groovy/team/fitin/dto/BodyInfoRequestDto.java
@@ -0,0 +1,22 @@
+package team.fitin.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class BodyInfoRequestDto {
+
+    @Schema(description = "사용자 키", example = "180.5")
+    private Float height;
+
+    @Schema(description = "사용자 몸무게", example = "75.0")
+    private Float weight;
+
+    @Schema(description = "어깨 너비", example = "45.2")
+    private Float shoulderWidth;
+
+    @Schema(description = "다리 길이", example = "105.0")
+    private Float legLength;
+}
